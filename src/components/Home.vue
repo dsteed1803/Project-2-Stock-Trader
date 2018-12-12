49997a7a6aa16064bd2206eb8cff774a191d436a
Dallin Steed <dallinsteed@mail.weber.edu>
2018-12-11 19:33:53 -0700

    Final Commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <h1>Trade or View your Portfolio</h1>
-    <h6>You may Save & Load your Data</h6>
-    <h6>Click on 'End Day' to begin a New Day!</h6>
+    <h1 class="header">Trade or View your Portfolio</h1>
+    <h6 class="subheader">You may Save & Load your Data</h6>
+    <h6 class="subheader">Click on 'End Day' to begin a New Day!</h6>
     <hr>
 
     <p>Your Funds:
@@ -23,3 +23,22 @@
     }
   }
 </script>
+
+<style>
+  .header {
+    background-color: lightsalmon;
+    border: 2pt;
+    font-family : Phosphate;
+    font-size : 60px;
+    color : #FFF7F7;
+    color : rgb(255, 247, 247);
+  }
+  .subheader {
+    background-color: beige;
+    font-family : PT Sans Narrow;
+    font-size: 25px;
+  }
+  .subheader2 {
+    background-color: cornflowerblue;
+  }
+</style>
